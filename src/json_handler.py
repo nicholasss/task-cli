96f61ba34e45767a55d26ae7f4cde5c43795c8b6
nicholasss <nicholaszingleman@gmail.com>
2024-09-06 23:52:38 -0400

    added json logic

--- src/json_handler.py
+++ src/json_handler.py
@@ -1,5 +1,6 @@
-import os, logging, time, uuid
+import os, logging, datetime
 from pathlib import Path
+from enum import Enum
 
 import orjson
 
@@ -8,6 +9,10 @@ DATA_DIR_NAME = "data"
 JSON_DATA_NAME = "todo_list.json"
 CURRENT_DIRECTORY = os.getcwd()
 
+class Status(Enum):
+	DONE = "done"
+	TODO = "todo"
+	IN_PROGRESS = "in-progress"
 
 logging.basicConfig(
 	level=logging.DEBUG,
@@ -31,22 +36,50 @@ def __create_data_dir():
 		logging.debug("Prexisting Data Directory")
 
 # write to file
-def add_item(item: str):
+def add_item(item_description: str) -> int:
 	"""Adds item to the JSON file."""
 	# ID, Description, status, createdAt, updatedAt
 	__create_data_dir()
+
+	current_utc_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
+	item_id: int
 
 	json_file_path = Path(f"{CURRENT_DIRECTORY}/{DATA_DIR_NAME}/{JSON_DATA_NAME}")
 	logging.debug(f"JSON File Path %% {json_file_path}")
 
 	if not json_file_path.exists():
 		logging.debug("Creating JSON file.")
+
+		item_id = 1
+		logging.debug(f"Item ID of {item_id} provided.")
+
+		item_status = Status.TODO
+		logging.debug(f"Status of {item_status} provided.")
+
+		item_dict = {
+			"id": item_id,
+			"desc": item_description,
+			"status": item_status,
+			"createdAt": current_utc_time,
+			"updatedAt": current_utc_time
+		}
+		file_dict = {"todo": item_dict}
+		json_data = orjson.dumps(file_dict)
+
 		with json_file_path.open("w") as file:
-			file.write(item)
+			file.write(json_data)
+
+		
 
 	else:
 		logging.debug("JSON file exists, appending item.")
 		# open, deserialize, add item to array, write to file
 
+		# item ID is updated to be correct when added
+		item_id = 2
+
+
+
+	return item_id
 
 # load file
