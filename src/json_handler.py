21f99a22c1151ae7bcf8cf6a95cb79ab15864658
nicholasss <nicholaszingleman@gmail.com>
2024-09-06 22:32:01 -0400

    importing orjson

--- src/json_handler.py
+++ src/json_handler.py
@@ -1,9 +1,15 @@
 import argparse
 import logging
 
+import orjson
+
 logging.basicConfig(
 	level=logging.DEBUG,
 	format="%(asctime)s %(levelname)s %(message)s"
 )
 
 logging.info("Initializing json_handler.py")
+
+# create file
+
+# load file
