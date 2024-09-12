53a2c11bf2a1cea85cfe3e8723f96ac6a6d76e54
nicholasss <nicholaszingleman@gmail.com>
2024-09-12 12:53:17 -0400

    formatted via lazyvim... and removed the circular import

--- src/json_handler.py
+++ src/json_handler.py
@@ -1,16 +1,22 @@
-import os, logging, datetime
-from pathlib import Path
+import datetime
+import logging
+import os
 from enum import Enum
-
+from pathlib import Path
 import orjson
 
+<<<<<<< HEAD
 
 LOGGING_LEVEL = logging.DEBUG
+=======
+>>>>>>> 90326ac (formatted via lazyvim... and removed the circular import)
 
+LOGGING_LEVEL = logging.DEBUG
 DATA_DIR_NAME = "data"
 CURRENT_DIRECTORY = os.getcwd()
 __JSON_DATA_NAME = "todo_list.json"
-JSON_FILE_PATH = Path(f"{CURRENT_DIRECTORY}/{DATA_DIR_NAME}/{__JSON_DATA_NAME}")
+JSON_FILE_PATH = Path(
+    f"{CURRENT_DIRECTORY}/{DATA_DIR_NAME}/{__JSON_DATA_NAME}")
 
 ID_FN = "id"
 DESCR_FN = "description"
@@ -20,13 +26,19 @@ UPDAT_FN = "updatedAt"
 
 
 class Status(Enum):
-	Done = "done"
-	To_Do = "todo"
-	In_Progress = "in-progress"
+    Done = "done"
+    To_Do = "todo"
+    In_Progress = "in-progress"
+
 
 logging.basicConfig(
+<<<<<<< HEAD
 	level=LOGGING_LEVEL,
 	format="%(asctime)s %(levelname)s %(message)s"
+=======
+    level=LOGGING_LEVEL,  # Set in main.py
+    format="%(asctime)s %(levelname)s %(message)s",
+>>>>>>> 90326ac (formatted via lazyvim... and removed the circular import)
 )
 
 logging.debug("Initializing json_handler.py")
@@ -34,122 +46,131 @@ logging.debug(f"Current Directory %% {CURRENT_DIRECTORY}")
 
 
 def __create_data_dir():
-	"""Creates the data directory './data' if it does not exist already."""
-	data_dir_path = Path(f"{CURRENT_DIRECTORY}/{DATA_DIR_NAME}")
-	logging.debug(f"Data Directory Path %% {data_dir_path}")
-	if not data_dir_path.is_dir():
-		os.mkdir(data_dir_path)
-		logging.debug("Created Data Directory.")
-	else:
-		logging.debug("Prexisting Data Directory")
+    """Creates the data directory './data' if it does not exist already."""
+    data_dir_path = Path(f"{CURRENT_DIRECTORY}/{DATA_DIR_NAME}")
+    logging.debug(f"Data Directory Path %% {data_dir_path}")
+    if not data_dir_path.is_dir():
+        os.mkdir(data_dir_path)
+        logging.debug("Created Data Directory.")
+    else:
+        logging.debug("Prexisting Data Directory")
 
-def __read_file_data(file_path: str) -> list[dict[str: any]]:
-	"""Reading arbitrary bytes to file."""
-	with file_path.open("rb") as file:
-			file_data = file.read()
-			file_dict = orjson.loads(file_data)
 
-			logging.debug(f"Opened file at path %% {file_path}")
-			return file_dict
+def __read_file_data(file_path: str) -> list[dict[str:any]]:
+    """Reading arbitrary bytes to file."""
+    with file_path.open("rb") as file:
+        file_data = file.read()
+        file_dict = orjson.loads(file_data)
 
-def __write_file_data(file_path: str, file_dict: list[dict[str: any]]) -> None:
-	"""Writing arbitrary bytes to file."""
-	with file_path.open("wb") as file:
-			file_data = orjson.dumps(file_dict)
-			file.write(file_data)
-			logging.debug(f"Wrote to file at path %% {file_path}")
+        logging.debug(f"Opened file at path %% {file_path}")
+        return file_dict
+
+
+def __write_file_data(file_path: str, file_dict: list[dict[str:any]]) -> None:
+    """Writing arbitrary bytes to file."""
+    with file_path.open("wb") as file:
+        file_data = orjson.dumps(file_dict)
+        file.write(file_data)
+        logging.debug(f"Wrote to file at path %% {file_path}")
+
 
 # write to file
 def add_item(item_description: str) -> int:
-	"""Adds item to the JSON file."""
-	# ID, Description, status, createdAt, updatedAt
-	__create_data_dir()
+    """Adds item to the JSON file."""
+    # ID, Description, status, createdAt, updatedAt
+    __create_data_dir()
 
-	current_utc_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    current_utc_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
-	logging.debug(f"JSON File Path %% {JSON_FILE_PATH}")
+    logging.debug(f"JSON File Path %% {JSON_FILE_PATH}")
 
-	item_dict = {
-			ID_FN: 0,
-			DESCR_FN: item_description,
-			STATU_FN: Status.To_Do, # Explicitly set to To_Do
-			CREAT_FN: current_utc_time,
-			UPDAT_FN: current_utc_time
-		}
+    item_dict = {
+        ID_FN: 0,
+        DESCR_FN: item_description,
+        STATU_FN: Status.To_Do,  # Explicitly set to To_Do
+        CREAT_FN: current_utc_time,
+        UPDAT_FN: current_utc_time,
+    }
 
-	if not JSON_FILE_PATH.exists():
-		logging.debug("Creating JSON file.")
+    if not JSON_FILE_PATH.exists():
+        logging.debug("Creating JSON file.")
 
-		item_dict[ID_FN] = 1
-		logging.debug(f"Item ID of {item_dict[ID_FN]} provided.")
-		logging.debug(f"Status of '{item_dict[STATU_FN]}' provided.")
+        item_dict[ID_FN] = 1
+        logging.debug(f"Item ID of {item_dict[ID_FN]} provided.")
+        logging.debug(f"Status of '{item_dict[STATU_FN]}' provided.")
 
-		file_array = [item_dict]
-		__write_file_data()(JSON_FILE_PATH, file_array)
-		
-		logging.debug("Wrote one item to new JSON file.")
+        file_array = [item_dict]
+        __write_file_data()(JSON_FILE_PATH, file_array)
 
-	else:
-		logging.debug("Opened file to append a new item.")
+        logging.debug("Wrote one item to new JSON file.")
 
-		previous_file_array = __read_file_data(JSON_FILE_PATH)
+    else:
+        logging.debug("Opened file to append a new item.")
 
-		highest_item_id = 1
-		for item in previous_file_array:
-			if item[ID_FN] >= highest_item_id:
-				highest_item_id = item[ID_FN] + 1
-		item_dict[ID_FN] = highest_item_id
+        previous_file_array = __read_file_data(JSON_FILE_PATH)
 
-		logging.debug(f"Item ID of {item_dict[ID_FN]} provided.")
-		logging.debug(f"Status of '{item_dict[STATU_FN]}' provided.")
+        highest_item_id = 1
+        for item in previous_file_array:
+            if item[ID_FN] >= highest_item_id:
+                highest_item_id = item[ID_FN] + 1
+        item_dict[ID_FN] = highest_item_id
 
-		previous_file_array.append(item_dict)
-		__write_file_data(JSON_FILE_PATH, previous_file_array)
+        logging.debug(f"Item ID of {item_dict[ID_FN]} provided.")
+        logging.debug(f"Status of '{item_dict[STATU_FN]}' provided.")
 
-	return item_dict[ID_FN]
+        previous_file_array.append(item_dict)
+        __write_file_data(JSON_FILE_PATH, previous_file_array)
+
+    return item_dict[ID_FN]
+
 
 def list_items() -> list[str]:
-	"""Prints out a list of items in the todo_list json file."""
-	if not JSON_FILE_PATH.exists():
-		print("No items are in the list.")
-	
-	else:
-		item_list = __read_file_data(JSON_FILE_PATH)
-		logging.debug(f"Read from item list %% {JSON_FILE_PATH}")
+    """Prints out a list of items in the todo_list json file."""
+    if not JSON_FILE_PATH.exists():
+        print("No items are in the list.")
 
-		print(" %%% Item List:")
-		for item in item_list:
-			print(f"ID: {item[ID_FN]}, Task: {item[DESCR_FN]}, Status: {item[STATU_FN]}")
+    else:
+        item_list = __read_file_data(JSON_FILE_PATH)
+        logging.debug(f"Read from item list %% {JSON_FILE_PATH}")
+
+        print(" %%% Item List:")
+        for item in item_list:
+            print(
+                f"ID: {item[ID_FN]}, Task: {
+                    item[DESCR_FN]}, Status: {item[STATU_FN]}"
+            )
+
 
 def update_item(item_id: int, item_desc: str) -> None:
-	"""Updates an item's description."""
-	logging.debug(f"Updating ID: {item_id} to Name '{item_desc}'")
-	
-	if not JSON_FILE_PATH.exists():
-		print("No items are in the list.")
-	
-	else:
-		item_list = __read_file_data(JSON_FILE_PATH)
-		logging.debug(f"Read from item list %% {JSON_FILE_PATH}")
+    """Updates an item's description."""
+    logging.debug(f"Updating ID: {item_id} to Name '{item_desc}'")
 
-		for item in item_list:
-			if item[ID_FN] == item_id:
-				item[DESCR_FN] = item_desc
+    if not JSON_FILE_PATH.exists():
+        print("No items are in the list.")
 
-		__write_file_data(JSON_FILE_PATH, item_list)
+    else:
+        item_list = __read_file_data(JSON_FILE_PATH)
+        logging.debug(f"Read from item list %% {JSON_FILE_PATH}")
+
+        for item in item_list:
+            if item[ID_FN] == item_id:
+                item[DESCR_FN] = item_desc
+
+        __write_file_data(JSON_FILE_PATH, item_list)
+
 
 def delete_item(item_id: int) -> None:
-	"""Deletes a specific item from the list."""
-	logging.debug(f"Deleting ID: {item_id}")
+    """Deletes a specific item from the list."""
+    logging.debug(f"Deleting ID: {item_id}")
 
-	if not JSON_FILE_PATH.exists():
-		print("No items are in the list.")
-	
-	else:
-		item_list = __read_file_data(JSON_FILE_PATH)
-		logging.debug(f"Read from item list %% {JSON_FILE_PATH}")
+    if not JSON_FILE_PATH.exists():
+        print("No items are in the list.")
 
-		for item in item_list:
-			if item[ID_FN] == item_id:
-				item_list.remove(item)
-		__write_file_data(JSON_FILE_PATH, item_list)
+    else:
+        item_list = __read_file_data(JSON_FILE_PATH)
+        logging.debug(f"Read from item list %% {JSON_FILE_PATH}")
+
+        for item in item_list:
+            if item[ID_FN] == item_id:
+                item_list.remove(item)
+        __write_file_data(JSON_FILE_PATH, item_list)
--- src/main.py
+++ src/main.py
@@ -7,39 +7,48 @@ import json_handler as jh
 LOGGING_LEVEL = logging.DEBUG
 
 
-logging.basicConfig(
-	level=LOGGING_LEVEL,
-	format="%(asctime)s %(levelname)s %(message)s"
-)
+logging.basicConfig(level=LOGGING_LEVEL,
+                    format="%(asctime)s %(levelname)s %(message)s")
 
 logging.debug("Initializing main.py")
-parser = argparse.ArgumentParser(description="To Do List CLI written in Python.")
+parser = argparse.ArgumentParser(
+    description="To Do List CLI written in Python.")
 
-parser.add_argument('-a', '--add', help="Add an item to the list.", metavar="Item")
-parser.add_argument('-l', '--list', help="List out all items on the list.", action="store_true")
-parser.add_argument('-u', '--update', help="Update description of specified task.", nargs=2, metavar=("ID", "Description"))
-parser.add_argument('-d', '--delete', help="Deletes specified task.", metavar='ID')
+parser.add_argument(
+    "-a", "--add", help="Add an item to the list.", metavar="Item")
+parser.add_argument(
+    "-l", "--list", help="List out all items on the list.", action="store_true"
+)
+parser.add_argument(
+    "-u",
+    "--update",
+    help="Update description of specified task.",
+    nargs=2,
+    metavar=("ID", "Description"),
+)
+parser.add_argument(
+    "-d", "--delete", help="Deletes specified task.", metavar="ID")
 
 args = parser.parse_args()
-logging.debug(f"Arguments passed are %% \"{args}\"")
+logging.debug(f'Arguments passed are %% "{args}"')
 
 if args.add:
-	logging.debug("'--add' argument found.")
-	added_item_id = jh.add_item(args.add)
-	print(f" %%% Item added successfully. (ID: {added_item_id})")
+    logging.debug("'--add' argument found.")
+    added_item_id = jh.add_item(args.add)
+    print(f" %%% Item added successfully. (ID: {added_item_id})")
 
 if args.list:
-	logging.debug("'--list' argument found.")
-	jh.list_items()
+    logging.debug("'--list' argument found.")
+    jh.list_items()
 
 if args.update:
-	logging.debug("'--update' argument found.")
-	if not args.update[0].isdigit():
-		logging.warning("First argument needs to be a task id.")
-	jh.update_item(int(args.update[0]), args.update[1])
+    logging.debug("'--update' argument found.")
+    if not args.update[0].isdigit():
+        logging.warning("First argument needs to be a task id.")
+    jh.update_item(int(args.update[0]), args.update[1])
 
 if args.delete:
-	logging.debug("'--delete' argument found.")
-	if not args.delete.isdigit():
-		logging.warning("'--delete' argument requires a task id.")
-	jh.delete_item(int(args.delete))
+    logging.debug("'--delete' argument found.")
+    if not args.delete.isdigit():
+        logging.warning("'--delete' argument requires a task id.")
+    jh.delete_item(int(args.delete))
